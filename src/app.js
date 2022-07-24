a0d1934a876d691b9319623a29608f5405db82a4
Shelby Davis <sjeneedavis@gmail.com>
2022-07-24 16:27:09 -0400

    add weather icon change

--- src/app.js
+++ src/app.js
@@ -21,12 +21,14 @@ function displayTemperature(response) {
 let humidityElement= document.querySelector("#humidity");
 let windElement= document.querySelector("#wind");
 let dateElement= document.querySelector("#date");
+let iconElement= document.querySelector("#icon");
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
     descriptionElement.innerHTML = response.data.weather[0].description;
     humidityElement.innerHTML = response.data.main.humidity;
     windElement.innerHTML = Math.round(response.data.wind.speed);
     dateElement.innerHTML = formatDate(response.data.dt * 1000);
+    iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 }
 
 let apiKey= "15fc459ac3739d95955d19e57ff4d582";
