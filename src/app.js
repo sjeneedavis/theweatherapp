6ebf7088faa3c1e70ed0c219b979e5233a228702
Shelby Davis <sjeneedavis@gmail.com>
2022-07-24 15:20:10 -0400

    remove console log

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ return `${day} ${hours}:${minutes}`;
 }
 
 function displayTemperature(response) {
-   console.log (response);
+   
     let temperatureElement = document.querySelector("#temperature");
     let cityElement = document.querySelector("#city");
     let descriptionElement = document.querySelector("#description");
